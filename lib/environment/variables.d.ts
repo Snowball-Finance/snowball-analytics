7249ef9736461854f4d9dfcfd46b348b87c79eea
mohsen <mhsnprvr4@gmail.com>
2021-12-23 00:00:34 +0330

    built

---
+++ lib/environment/variables.d.ts
@@ -0,0 +1,4 @@
+export declare const env: {
+    ANALYTICS_ENDPOINT: string | undefined;
+    ANALYTICS_SITE_ID: string | undefined;
+};
---
+++ lib/index.d.ts
@@ -0,0 +1,2 @@
+import MatomoTracker from '@datapunt/matomo-tracker-js';
+export declare const analytics: MatomoTracker;
